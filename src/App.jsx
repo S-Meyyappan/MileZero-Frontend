8ed2f1ae16e2cb5a189f878680dd78bf8016d019
S-Meyyappan <goatsforever0710@gmail.com>
2026-07-30 19:43:39 +0530

    Impl-Navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,22 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min';
+import 'bootstrap-icons/font/bootstrap-icons.css';
 
+
+import { Route, Routes } from 'react-router';
+
+import MainLayout from './pages/MainLayout';
+import Navbar from './components/Navbar';
+import PageNotFound from './pages/PageNotFound';
 
 function App() {
 
   return (
     <>
-    <h1>Hello Milezero !</h1>
+    <Routes>
+      <Route path = "/" element={<MainLayout/>}/>
+      <Route path = "*" element={<PageNotFound/>}/>
+    </Routes>
     </>
   )
 }
